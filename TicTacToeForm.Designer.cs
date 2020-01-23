a264269f85cd4fdae292dd203f5bcf0a32e60a69
Isaiah B. Lacey <iblacey@csusf.local>
2020-01-23 11:03:12 -0500

    Add project files.

---
+++ TicTacToeForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace TicTacToev1._0
+{
+   partial class TicTacToeForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.btnReset = new System.Windows.Forms.Button();
+         this.btnExit = new System.Windows.Forms.Button();
+         this.txtUltimate = new System.Windows.Forms.TextBox();
+         this.txtTicTacToe = new System.Windows.Forms.TextBox();
+         this.picChoice2 = new System.Windows.Forms.PictureBox();
+         this.picChoice3 = new System.Windows.Forms.PictureBox();
+         this.picChoice6 = new System.Windows.Forms.PictureBox();
+         this.picChoice9 = new System.Windows.Forms.PictureBox();
+         this.picChoice5 = new System.Windows.Forms.PictureBox();
+         this.picChoice4 = new System.Windows.Forms.PictureBox();
+         this.picChoice7 = new System.Windows.Forms.PictureBox();
+         this.picChoice8 = new System.Windows.Forms.PictureBox();
+         this.picChoice1 = new System.Windows.Forms.PictureBox();
+         this.picTicTacToe = new System.Windows.Forms.PictureBox();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice2)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice3)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice6)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice9)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice5)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice4)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice7)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice8)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice1)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picTicTacToe)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // btnReset
+         // 
+         this.btnReset.BackColor = System.Drawing.Color.Beige;
+         this.btnReset.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnReset.Location = new System.Drawing.Point(56, 445);
+         this.btnReset.Margin = new System.Windows.Forms.Padding(1);
+         this.btnReset.Name = "btnReset";
+         this.btnReset.Size = new System.Drawing.Size(102, 34);
+         this.btnReset.TabIndex = 10;
+         this.btnReset.Text = "Reset ";
+         this.btnReset.UseVisualStyleBackColor = false;
+         this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+         // 
+         // btnExit
+         // 
+         this.btnExit.BackColor = System.Drawing.Color.Beige;
+         this.btnExit.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnExit.Location = new System.Drawing.Point(385, 445);
+         this.btnExit.Margin = new System.Windows.Forms.Padding(1);
+         this.btnExit.Name = "btnExit";
+         this.btnExit.Size = new System.Drawing.Size(103, 34);
+         this.btnExit.TabIndex = 11;
+         this.btnExit.Text = "Exit";
+         this.btnExit.UseVisualStyleBackColor = false;
+         this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+         // 
+         // txtUltimate
+         // 
+         this.txtUltimate.BackColor = System.Drawing.Color.Beige;
+         this.txtUltimate.Font = new System.Drawing.Font("Algerian", 27.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtUltimate.Location = new System.Drawing.Point(145, 10);
+         this.txtUltimate.Margin = new System.Windows.Forms.Padding(1);
+         this.txtUltimate.Name = "txtUltimate";
+         this.txtUltimate.Size = new System.Drawing.Size(270, 57);
+         this.txtUltimate.TabIndex = 13;
+         this.txtUltimate.Text = "The Ultimate";
+         // 
+         // txtTicTacToe
+         // 
+         this.txtTicTacToe.BackColor = System.Drawing.Color.Beige;
+         this.txtTicTacToe.Font = new System.Drawing.Font("Algerian", 27.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtTicTacToe.Location = new System.Drawing.Point(158, 326);
+         this.txtTicTacToe.Margin = new System.Windows.Forms.Padding(1);
+         this.txtTicTacToe.Name = "txtTicTacToe";
+         this.txtTicTacToe.Size = new System.Drawing.Size(236, 57);
+         this.txtTicTacToe.TabIndex = 14;
+         this.txtTicTacToe.Text = "Tic Tac Toe";
+         // 
+         // picChoice2
+         // 
+         this.picChoice2.BackColor = System.Drawing.Color.White;
+         this.picChoice2.Location = new System.Drawing.Point(258, 103);
+         this.picChoice2.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice2.Name = "picChoice2";
+         this.picChoice2.Size = new System.Drawing.Size(38, 42);
+         this.picChoice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice2.TabIndex = 23;
+         this.picChoice2.TabStop = false;
+         this.picChoice2.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice3
+         // 
+         this.picChoice3.BackColor = System.Drawing.Color.White;
+         this.picChoice3.Location = new System.Drawing.Point(323, 103);
+         this.picChoice3.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice3.Name = "picChoice3";
+         this.picChoice3.Size = new System.Drawing.Size(38, 42);
+         this.picChoice3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice3.TabIndex = 22;
+         this.picChoice3.TabStop = false;
+         this.picChoice3.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice6
+         // 
+         this.picChoice6.BackColor = System.Drawing.Color.White;
+         this.picChoice6.Location = new System.Drawing.Point(323, 174);
+         this.picChoice6.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice6.Name = "picChoice6";
+         this.picChoice6.Size = new System.Drawing.Size(38, 42);
+         this.picChoice6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice6.TabIndex = 21;
+         this.picChoice6.TabStop = false;
+         this.picChoice6.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice9
+         // 
+         this.picChoice9.BackColor = System.Drawing.Color.White;
+         this.picChoice9.Location = new System.Drawing.Point(323, 245);
+         this.picChoice9.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice9.Name = "picChoice9";
+         this.picChoice9.Size = new System.Drawing.Size(38, 42);
+         this.picChoice9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice9.TabIndex = 20;
+         this.picChoice9.TabStop = false;
+         this.picChoice9.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice5
+         // 
+         this.picChoice5.BackColor = System.Drawing.Color.White;
+         this.picChoice5.Location = new System.Drawing.Point(258, 174);
+         this.picChoice5.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice5.Name = "picChoice5";
+         this.picChoice5.Size = new System.Drawing.Size(38, 42);
+         this.picChoice5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice5.TabIndex = 19;
+         this.picChoice5.TabStop = false;
+         this.picChoice5.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice4
+         // 
+         this.picChoice4.BackColor = System.Drawing.Color.White;
+         this.picChoice4.Location = new System.Drawing.Point(195, 174);
+         this.picChoice4.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice4.Name = "picChoice4";
+         this.picChoice4.Size = new System.Drawing.Size(38, 42);
+         this.picChoice4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice4.TabIndex = 18;
+         this.picChoice4.TabStop = false;
+         this.picChoice4.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice7
+         // 
+         this.picChoice7.BackColor = System.Drawing.Color.White;
+         this.picChoice7.Location = new System.Drawing.Point(195, 245);
+         this.picChoice7.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice7.Name = "picChoice7";
+         this.picChoice7.Size = new System.Drawing.Size(38, 42);
+         this.picChoice7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice7.TabIndex = 17;
+         this.picChoice7.TabStop = false;
+         this.picChoice7.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice8
+         // 
+         this.picChoice8.BackColor = System.Drawing.Color.White;
+         this.picChoice8.Location = new System.Drawing.Point(258, 245);
+         this.picChoice8.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice8.Name = "picChoice8";
+         this.picChoice8.Size = new System.Drawing.Size(38, 42);
+         this.picChoice8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice8.TabIndex = 16;
+         this.picChoice8.TabStop = false;
+         this.picChoice8.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picChoice1
+         // 
+         this.picChoice1.BackColor = System.Drawing.Color.White;
+         this.picChoice1.Location = new System.Drawing.Point(195, 103);
+         this.picChoice1.Margin = new System.Windows.Forms.Padding(1);
+         this.picChoice1.Name = "picChoice1";
+         this.picChoice1.Size = new System.Drawing.Size(38, 42);
+         this.picChoice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picChoice1.TabIndex = 15;
+         this.picChoice1.TabStop = false;
+         this.picChoice1.Click += new System.EventHandler(this.ButtonClicked);
+         // 
+         // picTicTacToe
+         // 
+         this.picTicTacToe.Image = global::TicTacToev1._0.Resource1.ticTacToeBackground1;
+         this.picTicTacToe.Location = new System.Drawing.Point(172, 75);
+         this.picTicTacToe.Margin = new System.Windows.Forms.Padding(1);
+         this.picTicTacToe.Name = "picTicTacToe";
+         this.picTicTacToe.Size = new System.Drawing.Size(210, 239);
+         this.picTicTacToe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.picTicTacToe.TabIndex = 12;
+         this.picTicTacToe.TabStop = false;
+         // 
+         // TicTacToeForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.AutoSize = true;
+         this.BackColor = System.Drawing.Color.Brown;
+         this.BackgroundImage = global::TicTacToev1._0.Resource1.TicTacToeBackground2;
+         this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+         this.ClientSize = new System.Drawing.Size(609, 560);
+         this.Controls.Add(this.picChoice2);
+         this.Controls.Add(this.picChoice3);
+         this.Controls.Add(this.picChoice6);
+         this.Controls.Add(this.picChoice9);
+         this.Controls.Add(this.picChoice5);
+         this.Controls.Add(this.picChoice4);
+         this.Controls.Add(this.picChoice7);
+         this.Controls.Add(this.picChoice8);
+         this.Controls.Add(this.picChoice1);
+         this.Controls.Add(this.txtTicTacToe);
+         this.Controls.Add(this.txtUltimate);
+         this.Controls.Add(this.btnExit);
+         this.Controls.Add(this.btnReset);
+         this.Controls.Add(this.picTicTacToe);
+         this.ForeColor = System.Drawing.SystemColors.ControlText;
+         this.Margin = new System.Windows.Forms.Padding(1);
+         this.Name = "TicTacToeForm";
+         this.Text = "TicTacToe";
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice2)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice3)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice6)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice9)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice5)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice4)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice7)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice8)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picChoice1)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.picTicTacToe)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+      private System.Windows.Forms.Button btnReset;
+      private System.Windows.Forms.Button btnExit;
+      private System.Windows.Forms.PictureBox picTicTacToe;
+      private System.Windows.Forms.TextBox txtUltimate;
+      private System.Windows.Forms.TextBox txtTicTacToe;
+      private System.Windows.Forms.PictureBox picChoice1;
+      private System.Windows.Forms.PictureBox picChoice8;
+      private System.Windows.Forms.PictureBox picChoice7;
+      private System.Windows.Forms.PictureBox picChoice4;
+      private System.Windows.Forms.PictureBox picChoice5;
+      private System.Windows.Forms.PictureBox picChoice9;
+      private System.Windows.Forms.PictureBox picChoice6;
+      private System.Windows.Forms.PictureBox picChoice3;
+      private System.Windows.Forms.PictureBox picChoice2;
+   }
+}
+
---
+++ TicTacToeForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToev1._0
+{
+
+   /*
+    * Isaiah Lacey
+    * 1-21-20
+    * Tic Tac Toe
+    * 
+    */
+
+   public partial class TicTacToeForm : Form
+   {
+
+      int turnCounter;
+    
+
+
+      public TicTacToeForm()
+      {
+         InitializeComponent();
+      }
+      
+      private void ButtonClicked(object sender, EventArgs e)
+      {
+         //Cast object 
+         PictureBox xOrOButton = (PictureBox)sender;
+
+         //Decide which player's (X or O) turn it is. 
+         if (turnCounter == 0)
+         {
+            xOrOButton.Image = Resource1.x2;
+            
+            turnCounter++;
+         }
+         else
+         {
+            xOrOButton.Image = Resource1.o4;
+            turnCounter--;
+         }
+
+         //Disable button upon getting clicked so no change can occur
+         xOrOButton.Enabled = false;
+
+         
+
+      }
+
+      private void btnReset_Click(object sender, EventArgs e)
+      {
+         //restting the turn to begginning 
+         turnCounter = 0;
+
+       
+
+         //clearing the X's and O's
+         picChoice1.Image = null;
+         picChoice2.Image = null;
+         picChoice3.Image = null;
+         picChoice4.Image = null;
+         picChoice5.Image = null;
+         picChoice6.Image = null;
+         picChoice7.Image = null;
+         picChoice8.Image = null;
+         picChoice9.Image = null;
+
+         //Re enabling the picture boxes 
+         picChoice1.Enabled = true;
+         picChoice2.Enabled = true;
+         picChoice3.Enabled = true;
+         picChoice4.Enabled = true;
+         picChoice5.Enabled = true;
+         picChoice6.Enabled = true;
+         picChoice7.Enabled = true;
+         picChoice8.Enabled = true;
+         picChoice9.Enabled = true;
+
+
+      }
+
+      private void btnExit_Click(object sender, EventArgs e)
+      {
+         //Close the program on the click of the exit button
+         this.Close();
+      }
+
+   }
+}
