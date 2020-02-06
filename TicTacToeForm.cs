8bff5b7317a23be1206c555148ca06a16923dbb3
Isaiah Lacey <laceyi2025@gmail.com>
2020-02-06 18:37:11 -0500

    Updated phase 2

--- TicTacToeForm.cs
+++ TicTacToeForm.cs
@@ -37,6 +37,53 @@ namespace TicTacToev1._0
          return (turnCounter == 0) ? Resource1.x2 : Resource1.o4;
       }
 
+      private int sumOfPictureBoxRow(int rowAmount, ref int[,] amountToCheck)
+      {
+         int sum = 0;
+
+         for (int column = 0; column < amountToCheck.GetLength(1); column++)
+         {
+            sum += amountToCheck[rowAmount, column];
+         }
+
+         return sum;
+      }
+
+      private int sumOfPictureBoxColumn(int columnAmount, ref int[,] amountToCheck)
+      {
+         int sum = 0;
+
+         for (int row = 0; row < amountToCheck.GetLength(1); row++)
+         {
+            sum += amountToCheck[row, columnAmount];
+         }
+
+         return sum;
+      }
+
+
+      private void CheckForWinner(int amountToCheck)
+      {
+
+         sumOfPictureBoxRow(0, ref theBoard);
+         sumOfPictureBoxRow(1, ref theBoard);
+         sumOfPictureBoxRow(2, ref theBoard);
+         sumOfPictureBoxColumn(0, ref theBoard);
+         sumOfPictureBoxColumn(1, ref theBoard);
+         sumOfPictureBoxColumn(2, ref theBoard);
+
+         if (amountToCheck == 30)
+         {
+            MessageBox.Show("X is the WINNER!!!!!");
+            MassPitcureBoxEnableorDisable(false);
+         }
+         else if (amountToCheck == 300)
+         {
+            MessageBox.Show("O is the WINNER!!!!!");
+            MassPitcureBoxEnableorDisable(false);
+         }
+
+      }
 
       private void ButtonClicked(object sender, EventArgs e)
       {
@@ -54,6 +101,10 @@ namespace TicTacToev1._0
             xOrOButton.Image = SetImage();
 
             theBoard[row, column] = 10;
+
+            
+
+
          }
          else
          {
